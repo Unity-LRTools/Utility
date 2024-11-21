8c7deef99adf02576b0f687fa409451c05e14bb7
Unity-LRTools <pro.lucasrossignol@gmail.com>
2024-11-21 15:22:42 +0100

    Add button custom styling

--- Editor/CustomGUIStyle.cs
+++ Editor/CustomGUIStyle.cs
@@ -4,6 +4,7 @@ namespace LRT.Utility.Editor
 {
 	public static class CustomGUIStyle
 	{
+		#region Label
 		/// <summary>
 		/// Override the default label skin to create a new style
 		/// </summary>
@@ -65,5 +66,82 @@ namespace LRT.Utility.Editor
 
 			return style;
 		}
+		#endregion
+
+		#region Button
+		public static GUIStyle Button(
+			Font font = null,
+			int fontSize = 0,
+			FontStyle fontStyle = FontStyle.Normal,
+			TextAnchor alignment = TextAnchor.MiddleCenter,
+			bool wordWrap = false,
+			Color? normalTextColor = null,
+			Color? hoverTextColor = null,
+			Color? activeTextColor = null,
+			Color? focusedTextColor = null,
+			Color? onNormalTextColor = null,
+			Color? onHoverTextColor = null,
+			Color? onActiveTextColor = null,
+			Color? onFocusedTextColor = null,
+			Texture2D normalBackground = null,
+			Texture2D hoverBackground = null,
+			Texture2D activeBackground = null,
+			Texture2D focusedBackground = null,
+			Texture2D onNormalBackground = null,
+			Texture2D onHoverBackground = null,
+			Texture2D onActiveBackground = null,
+			Texture2D onFocusedBackground = null,
+			RectOffset border = null,
+			RectOffset margin = null,
+			RectOffset padding = null,
+			RectOffset overflow = null,
+			bool stretchWidth = true,
+			bool stretchHeight = false
+		)
+		{
+			// Use default button skin
+			GUIStyle baseStyle = GUI.skin.button;
+
+			// Create a new style
+			GUIStyle customStyle = new GUIStyle(baseStyle)
+			{
+				font = font ?? baseStyle.font,
+				fontSize = fontSize > 0 ? fontSize : baseStyle.fontSize,
+				fontStyle = fontStyle,
+				alignment = alignment,
+				wordWrap = wordWrap,
+				stretchWidth = stretchWidth,
+				stretchHeight = stretchHeight
+			};
+
+			// Assign text color
+			customStyle.normal.textColor = normalTextColor ?? baseStyle.normal.textColor;
+			customStyle.hover.textColor = hoverTextColor ?? baseStyle.hover.textColor;
+			customStyle.active.textColor = activeTextColor ?? baseStyle.active.textColor;
+			customStyle.focused.textColor = focusedTextColor ?? baseStyle.focused.textColor;
+			customStyle.onNormal.textColor = onNormalTextColor ?? baseStyle.onNormal.textColor;
+			customStyle.onHover.textColor = onHoverTextColor ?? baseStyle.onHover.textColor;
+			customStyle.onActive.textColor = onActiveTextColor ?? baseStyle.onActive.textColor;
+			customStyle.onFocused.textColor = onFocusedTextColor ?? baseStyle.onFocused.textColor;
+
+			// Assign texture background
+			customStyle.normal.background = normalBackground ?? baseStyle.normal.background;
+			customStyle.hover.background = hoverBackground ?? baseStyle.hover.background;
+			customStyle.active.background = activeBackground ?? baseStyle.active.background;
+			customStyle.focused.background = focusedBackground ?? baseStyle.focused.background;
+			customStyle.onNormal.background = onNormalBackground ?? baseStyle.onNormal.background;
+			customStyle.onHover.background = onHoverBackground ?? baseStyle.onHover.background;
+			customStyle.onActive.background = onActiveBackground ?? baseStyle.onActive.background;
+			customStyle.onFocused.background = onFocusedBackground ?? baseStyle.onFocused.background;
+
+			// Assign rect offset
+			customStyle.border = border ?? new RectOffset(baseStyle.border.left, baseStyle.border.right, baseStyle.border.top, baseStyle.border.bottom);
+			customStyle.margin = margin ?? new RectOffset(baseStyle.margin.left, baseStyle.margin.right, baseStyle.margin.top, baseStyle.margin.bottom);
+			customStyle.padding = padding ?? new RectOffset(baseStyle.padding.left, baseStyle.padding.right, baseStyle.padding.top, baseStyle.padding.bottom);
+			customStyle.overflow = overflow ?? new RectOffset(baseStyle.overflow.left, baseStyle.overflow.right, baseStyle.overflow.top, baseStyle.overflow.bottom);
+
+			return customStyle;
+		}
+		#endregion
 	}
 }
