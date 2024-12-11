e09d6d1ff2a1171b4c3cb17bfdca31554fbb1743
Unity-LRTools <pro.lucasrossignol@gmail.com>
2024-12-11 08:13:19 +0100

    Fix lazy singleton not working

--- Runtime/Singletons/LazySingletonScriptableObject.cs
+++ Runtime/Singletons/LazySingletonScriptableObject.cs
@@ -1,12 +1,14 @@
+using System;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
 public class LazySingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
 {
-	public static readonly string LOAD_PATH = "Assets/Resources/";
-	public static readonly string FILE_PATH = Path.Combine(LOAD_PATH, typeof(T).Name + ".asset");
-	public static readonly string RUNTIME_FILE_PATH = typeof(T).Name;
+	public static readonly string DEFAULT_EDITOR_LOAD_PATH = "Assets/Resources/";
+	public static readonly string EDITOR_FILE_NAME = typeof(T).Name + ".asset";
+	public static readonly string RUNTIME_FILE_NAME = typeof(T).Name;
 
 	public static T Instance
 	{
@@ -15,29 +17,50 @@ public class LazySingletonScriptableObject<T> : ScriptableObject where T : Scrip
 #if UNITY_EDITOR
 			if (!Application.isPlaying && instance == null)
 			{
-				instance = AssetDatabase.LoadAssetAtPath<T>(FILE_PATH);
+				string editorPath = GetEditorPath();
+				string editorFilepath = GetEditorFilepath();
+
+				instance = AssetDatabase.LoadAssetAtPath<T>(editorFilepath);
 				if (instance == null)
 				{
-					if (!Directory.Exists(LOAD_PATH))
-						Directory.CreateDirectory(LOAD_PATH);
+					if (!Directory.Exists(editorPath))
+						Directory.CreateDirectory(editorPath);
 
 					T bufferInstance = CreateInstance<T>();
-					AssetDatabase.CreateAsset(bufferInstance, FILE_PATH);
+					AssetDatabase.CreateAsset(bufferInstance, editorFilepath);
 					AssetDatabase.SaveAssets();
 					instance = bufferInstance;
 				}
 			}
-			else if(instance == null)
+			else if (instance == null)
 			{
-				instance = Resources.Load<T>(RUNTIME_FILE_PATH);
+				instance = Resources.Load<T>(RUNTIME_FILE_NAME);
 			}
 #else
 			if (instance == null)
-				instance = Resources.Load<T>(RUNTIME_FILE_PATH);
+				instance = Resources.Load<T>(RUNTIME_FILE_NAME);
 #endif
 			return instance;
 		}
 	}
 
 	private static T instance;
+
+	private static string GetEditorPath()
+	{
+		string filepath = DEFAULT_EDITOR_LOAD_PATH;
+
+		BindingFlags flags = BindingFlags.Static | BindingFlags.NonPublic;
+		MethodInfo getPathMethod = typeof(T).GetMethod("GetPath", flags);
+
+		if (getPathMethod != null)
+			filepath = (string)getPathMethod.Invoke(null, null);
+
+		return filepath;
+	}
+
+	private static string GetEditorFilepath()
+	{
+		return Path.Combine(GetEditorPath(), EDITOR_FILE_NAME);
+	}
 }
