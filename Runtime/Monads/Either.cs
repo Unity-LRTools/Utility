3c45c3cc28b759a4391b674f2cd3e365639e0425
Unity-LRTools <pro.lucasrossignol@gmail.com>
2024-12-20 12:35:18 +0100

    Add Either Monads following https://www.youtube.com/@git-amend video

---
+++ Runtime/Monads/Either.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+
+public struct Either<TLeft, TRight>
+{
+	TLeft left;
+	TRight right;
+	bool isRight;
+
+	Either(TLeft left, TRight right, bool isRight)
+	{
+		this.left = left;
+		this.right = right;
+		this.isRight = isRight;
+	}
+
+	public bool IsLeft => !isRight;
+	public bool IsRight => isRight;
+
+	public TLeft Left
+	{
+		get
+		{
+			if (IsRight)
+				throw new InvalidOperationException("No left value present.");
+
+			return left;
+		}
+	}
+
+	public TRight Right
+	{
+		get
+		{
+			if (IsLeft)
+				throw new InvalidOperationException("No right value present.");
+
+			return right;
+		}
+	}
+
+	public static Either<TLeft, TRight> FromLeft(TLeft left) => new(left, default, false);
+	public static Either<TLeft, TRight> FromRight(TRight right) => new(default, right, true);
+
+	public TResult Match<TResult>(Func<TLeft, TResult> leftFunc, Func<TRight, TResult> rightFunc)
+	{
+		return IsRight ? rightFunc(right) : leftFunc(left);
+	}
+
+	public void Do(Action<TLeft> leftFunc, Action<TRight> rightFunc)
+	{
+		if (IsRight)
+			rightFunc(right);
+		else
+			leftFunc(left);
+	}
+
+
+	public Either<TLeft, TResult> Select<TResult>(Func<TRight, TResult> map)
+	{
+		return IsRight ? Either<TLeft, TResult>.FromRight(map(right)) : Either<TLeft, TResult>.FromLeft(left);
+	}
+
+	public Either<TLeft, TResult> SelectMany<TResult>(Func<TRight, Either<TLeft, TResult>> bind)
+	{
+		return IsRight ? bind(right) : Either<TLeft, TResult>.FromLeft(left);
+	}
+
+	public static implicit operator Either<TLeft, TRight>(TLeft left) => FromLeft(left);
+	public static implicit operator Either<TLeft, TRight>(TRight right) => FromRight(right);
+
+	public override string ToString() => IsRight ? $"Right({right})" : $"Left({left})";
+}
